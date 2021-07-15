775ac236f8807949e665a0fe93d3ceac6bd29e79
malrok <themalrok@gmail.com>
2021-07-15 12:30:44 +0200

    the ship can no longer get out of screen

--- lib/components/player/ship.dart
+++ lib/components/player/ship.dart
@@ -2,10 +2,12 @@ import 'dart:math';
 import 'dart:ui';
 
 import 'package:flame/components.dart';
+import 'package:flame/geometry.dart';
 import 'package:flame/palette.dart';
+import 'package:shmup/engine/game.dart';
 
-class PlayerShip extends PositionComponent implements JoystickListener {
-  static const speed = 64.0;
+class PlayerShip extends PositionComponent with Hitbox, Collidable implements JoystickListener, HasGameRef<ShmupGame> {
+  static const speed = 128.0;
   static const squareSize = 12.0;
 
   double currentSpeed = 0;
@@ -13,9 +15,11 @@ class PlayerShip extends PositionComponent implements JoystickListener {
 
   static Paint white = BasicPalette.white.paint();
 
-  PlayerShip() {
-    x = 100;
-    y = 100;
+  PlayerShip(this.gameRef) {
+    x = this.gameRef.canvasSize.x / 2;
+    y = this.gameRef.canvasSize.y - this.gameRef.canvasSize.y * 0.1;
+    
+    addShape(HitboxRectangle());
   }
 
   @override
@@ -58,4 +62,20 @@ class PlayerShip extends PositionComponent implements JoystickListener {
     final delta = Vector2(cos(angle), sin(angle)) * (currentSpeed * dt);
     position.add(delta);
   }
+
+  @override
+  void onCollision(Set<Vector2> points, Collidable other) {
+    if (other is ScreenCollidable) {
+      if (this.x < 0) this.x = 0;
+      if (this.x > other.width) this.x = other.width;
+      if (this.y < 0) this.y = 0;
+      if (this.y > this.gameRef.maxY()) this.y = other.height;
+    }
+  }
+
+  @override
+  ShmupGame gameRef;
+
+  @override
+  bool get hasGameRef => true;
 }
--- lib/engine/game.dart
+++ lib/engine/game.dart
@@ -1,48 +1,29 @@
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
-import 'package:flame/gestures.dart';
 import 'package:shmup/components/player/ship.dart';
 import 'package:shmup/engine/joystick.dart';
 
-class ShmupGame extends BaseGame with MultiTouchDragDetector, HasDraggableComponents {
+class ShmupGame extends BaseGame with HasDraggableComponents, HasCollidables {
   bool running = true;
 
-  late PlayerShip _playerShip = PlayerShip();
+  late PlayerShip _playerShip;
   late Joystick _joystick;
 
   @override
   Future<void> onLoad() async {
+    print(this.canvasSize);
+
+    _playerShip = PlayerShip(this);
+
     _joystick = Joystick(this);
     _joystick.addObserver(_playerShip);
 
     add(_playerShip);
     add(_joystick);
+    add(ScreenCollidable());
   }
 
-  // @override
-  // void onReceiveDrag(DragUpdateInfo drag) {
-  //   joystick.onReceiveDrag(drag);
-  //   super.onReceiveDrag(drag);
-  // }
-
-  // @override
-  // void onPanUpdate(DragUpdateInfo info) {
-  //   final delta = details.delta;
-  //   final size = game.screenSize
-  //   double translateX = delta.dx;
-  //   double translateY = delta.dy;
-  //   // Make sure that the player never goes outside of the screen in the X-axis
-  //   if (playerRect.right + delta.dx >= size.width) {
-  //     translateX = size.width - playerRect.right;
-  //   } else if (playerRect.left + delta.dx <= 0) {
-  //     translateX = -playerRect.left;
-  //   }
-  //   // Make sure that the player never goes outside of the screen in the Y-axis
-  //   if (playerRect.bottom + delta.dy >= size.height) {
-  //     translateY = size.height - playerRect.bottom;
-  //   } else if (playerRect.top + delta.dy <= 0) {
-  //     translateY = -playerRect.top;
-  //   }
-  //   playerRect = playerRect.translate(translateX, translateY)
-  // }
+  double maxY() {
+    return this.canvasSize.y; // - _joystick.;
+  }
 }
